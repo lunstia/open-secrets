64b30395dad101be808047ca9ff7a90c1ee6393d
lunstia <136163666+lunstia@users.noreply.github.com>
2023-08-09 19:08:09 -0400

    Edit Post schema

--- models/Post.js
+++ models/Post.js
@@ -4,7 +4,9 @@ const Schema = mongoose.Schema;
 const Post = new Schema({
     user: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
     post: {type: String, required: true},
-    date: {type: Date, default: Date.now}
+    date: {type: Date, default: Date.now},
+    hideAuthor: {type: Boolean, default: true},
+    hidePost: {type: Boolean, default: false}
 });
 
 module.exports = mongoose.model('Post', Post);
